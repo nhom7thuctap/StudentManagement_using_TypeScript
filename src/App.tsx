2e35e12ee15a14cef4405871ad68f478e0ee8dd2
NghiaMa <nghiama0029@gmail.com>
2021-11-09 18:01:18 +0700

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import cityApi from 'api/cityApi';
+import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
 
 function App() {
+  useEffect(() => {
+    cityApi.getAll().then((response) => console.log(response));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Switch>
+        <Route path="/login"></Route>
+        <Route path="/admin"></Route>
+        <Route></Route>
+      </Switch>
     </div>
   );
 }
